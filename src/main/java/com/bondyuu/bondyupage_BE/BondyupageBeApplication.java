9a859320ed0412b9904c11b5eb1fc80d2a64e5b8
bondyuu <leo4902@gmail.com>
2022-10-28 13:05:11 +0900

    first commit

---
+++ src/main/java/com/bondyuu/bondyupage_BE/BondyupageBeApplication.java
@@ -0,0 +1,13 @@
+package com.bondyuu.bondyupage_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondyupageBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondyupageBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bondyuu/bondyupage_BE/BondyupageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondyuu.bondyupage_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondyupageBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
